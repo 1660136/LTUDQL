5a6b5dce707af6873ef123ca7e26f6c2402e9b05
dohongan <45332448+dohongan@users.noreply.github.com>
2019-01-04 17:29:46 -0800

    frist commit
    Cập nhật class Provider để giao tiếp với CSDL

---
+++ DALTUDQL_1660007_1660010_1660136/DALTUDQL_1660007_1660010_1660136/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DALTUDQL_1660007_1660010_1660136
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,9 @@
+﻿# 1660007_1660010_1660136_DeTaiQuanLypPhongMachTu_LTUDQL1
+-Đề Tài: Quản Lý Phòng Mạch Tư
+-Thông tin thành viên:
+  Họ và Tên		| MSSV    | Lớp
+  Đỗ Hồng Ân		| 1660007 | 16CK1
+  Nguyễn Duy Anh	| 1660010 | 16CK1
+  Nguyễn Thanh Duy	| 1660136 | 16CK3
+-Link Git: https://github.com/1660136/LTUDQL
+-Link Google Docs: https://docs.google.com/document/d/14GVztosmqVdwb5fGtHB9l8Mq7-Q7D_yul0qQtY1UeHw/edit?usp=sharing
